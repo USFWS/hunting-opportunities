8eaeff5b773d338ca05b10e195d6e66e28ecdf8f
Roy Hewitt <roy_hewitt@fws.gov>
2020-01-02 13:04:53 -0500

    Log CMT service errors

--- src/js/CMTService.js
+++ src/js/CMTService.js
@@ -2,7 +2,8 @@ const API_URL = 'https://systems.fws.gov/cmt/getOrgsGeo.do';
 
 const getByOrgCode = (code) => fetch(`${API_URL}?orgCode=${code}`)
   .then((res) => res.json())
-  .then((facility) => facility.features[0]);
+  .then((facility) => facility.features[0])
+  .catch(console.log);
 
 module.exports = {
   getByOrgCode,
