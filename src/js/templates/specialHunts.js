5e19a76677eba5f1f2be2aadeb22ef6e78cd7b97
Roy Hewitt <roy_hewitt@fws.gov>
2020-01-03 14:16:22 -0500

    Use standard method of displaying hunt regs, make zoom button SVG icon

--- src/js/templates/specialHunts.js
+++ src/js/templates/specialHunts.js
@@ -1,15 +1,24 @@
-const { capitalize } = require('../helpers');
+const { capitalize, matchesStateRegs } = require('../helpers');
 
-const createHuntItem = ({ properties: hunt }) => `
-  <h3>${hunt.OrgName}</h3>
-  <p class="hunt-unit-info">${hunt.Label}</p>
-  <button class="zoom-to-hunt-unit" value="${hunt.OBJECTID}">Zoom</button>
-  <ul>
-    <li>Method of take: ${hunt.MethodOfTake}</li>
-    <li>Date/time: ${hunt.DateTime}</li>
-    <li>Bag limit: ${hunt.BagLimits}</li>
-  </ul>
-`;
+const createHuntItem = ({ properties: hunt }) => {
+  // TODO: We need to include the HuntUrl here from the point dataset
+  console.log(hunt);
+  const regs = [hunt.MethodOfTake, hunt.DateTime, hunt.BagLimits].map(matchesStateRegs);
+
+  return `
+    <h3>${hunt.OrgName}</h3>
+    <p class="hunt-unit-info">${hunt.Label}</p>
+    <button class="zoom-to-hunt-unit veteran-zoom-to" value="${hunt.OBJECTID}">
+      <img class="facility-icon" src="./images/zoom.svg" alt="A zoom icon; click to zoom the map to the refuge's location" title="Zoom to Refuge"/>
+    </button>
+    <ul class="huntable-species-list">
+      ${regs.every((r) => r === true) ? '<li>All state regulations apply</li>' : ''}
+      ${regs[0] ? '' : `<li>Method of take: ${hunt.MethodOfTake}</li>`}
+      ${regs[1] ? '' : `<li>Date & times: ${hunt.DateTime}</li>`}
+      ${regs[2] ? '' : `<li>Bag limit: ${hunt.BagLimits}</li>`}
+    </ul>
+  `;
+};
 
 const template = (hunts, query) => `
   <h2>${capitalize(query)} hunts</h2>
