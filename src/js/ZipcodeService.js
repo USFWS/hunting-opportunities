c53784d90207687d19f2b532876e9759e701d414
Roy Hewitt <roy_hewitt@fws.gov>
2020-01-03 15:33:39 -0500

    Remove catch statement; handle in results

--- src/js/ZipcodeService.js
+++ src/js/ZipcodeService.js
@@ -1,8 +1,7 @@
 const API_URL = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_ZIP_Code_Points_analysis/FeatureServer/0/';
 
 const getZipCode = (code) => fetch(`${API_URL}query?outFields=PO_NAME%2CSTATE&f=pgeojson&where=ZIP_CODE='${code}'`)
-  .then((res) => res.json())
-  .catch(console.log);
+  .then((res) => res.json());
 
 module.exports = {
   getZipCode,
