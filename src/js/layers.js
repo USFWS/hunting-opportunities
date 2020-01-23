d0f57463d634e6826d5a3b7592c616dc01c62fca
Roy Hewitt <roy_hewitt@fws.gov>
2020-01-23 14:45:20 -0500

    Include imagery layer from ESRI

--- src/js/layers.js
+++ src/js/layers.js
@@ -5,6 +5,10 @@ require('esri-leaflet-renderers');
 const emitter = require('./emitter');
 const { getRelatedHuntableSpecies } = require('./HuntService');
 
+const imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+});
+
 const natGeo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
   maxZoom: 16,
@@ -54,6 +58,7 @@ const basemaps = {
 
 module.exports = {
   natGeo,
+  imagery,
   grayCanvas,
   refuges,
   basemaps,
