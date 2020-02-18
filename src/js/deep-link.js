c7f5ee0c88409f36e91cda5f1f11a9ddb6b4c86a
Roy Hewitt <roy_hewitt@fws.gov>
2020-02-18 15:27:28 -0500

    Destructure cameCase function; return bounds from getBounds function instead of emitting event directly

--- src/js/deep-link.js
+++ src/js/deep-link.js
@@ -1,6 +1,6 @@
 const L = require('leaflet');
 const querystring = require('query-string');
-const camelCase = require('camel-case');
+const { camelCase } = require('camel-case');
 
 const emitter = require('./emitter');
 const GLOBAL_BOUNDS = require('./bounds');
@@ -10,17 +10,13 @@ const stateToBounds = (state) => GLOBAL_BOUNDS[camelCase(state)];
 const boundsReducer = (bounds, val) => bounds.extend(val);
 
 const getBounds = (state) => {
-  if (typeof state === 'string') emitter.emit('set:bounds', GLOBAL_BOUNDS[camelCase(state)]);
+  if (typeof state === 'string') return GLOBAL_BOUNDS[camelCase(state)];
   if (Array.isArray(state)) {
     // ToDo: Error handling -- show a message if zoom doesn't work
-    const stateBounds = state.map(stateToBounds).reduce(boundsReducer, L.latLngBounds());
-    emitter.emit('set:bounds', stateBounds);
+    return state
+      .map(stateToBounds)
+      .reduce(boundsReducer, L.latLngBounds());
   }
-  return false;
-};
-
-const isState = (obj) => {
-  if (obj.state) getBounds(obj.state);
   return false;
 };
 
@@ -33,9 +29,9 @@ const processQueryString = (qs) => {
   const query = isQuery(parsed);
   const searchMethod = isSearchMethod(parsed);
 
-  if (isState(parsed)) getBounds(parsed);
   if (query) emitter.emit('query', query);
   if (searchMethod) emitter.emit('method', searchMethod);
+  if (parsed.state) emitter.emit('set:bounds', getBounds(parsed.state));
 };
 
 module.exports = {
