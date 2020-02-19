40fb68080174b184166dd0f4397ffb3b106d606f
Roy Hewitt <roy_hewitt@fws.gov>
2020-02-19 15:24:14 -0500

    Set value of input rather than sim click event; helper function returns input, not input value.

--- src/js/search/Search.js
+++ src/js/search/Search.js
@@ -14,49 +14,48 @@ const Search = function (opts) {
   this.stateSelect.addEventListener('input', this.emitQuery.bind(this));
   this.specialSelect.addEventListener('input', this.emitQuery.bind(this));
   this.speciesSelect.addEventListener('input', this.emitQuery.bind(this));
-  this.radios.forEach((r) => r.addEventListener('click', this.toggleSearchInterface.bind(this)));
+  this.radios.forEach((r) => r.addEventListener('click', (e) => this.toggleSearchInterface(e.target.value)));
 
   // Analytics events
   this.input.addEventListener('input', debounce((e) => { emitter.emit('search:term', e.target.value); }, 2500));
 
-  emitter.on('query', (query) => {
-    this.input.value = query;
-    this.input.dispatchEvent(new Event('input', { bubbles: true }));
-  });
-  emitter.on('method', (method) => {
-    const radioButton = this.radios.filter((r) => r.value === method);
-    if (radioButton[0]) radioButton[0].click();
+  // Select the appropriate radio button based on an updated query parameter
+  emitter.on('update:search', (params) => {
+    const radioButton = this.radios.filter((r) => r.value === params.method);
+    if (radioButton[0]) radioButton[0].checked = true;
   });
 };
 
 Search.prototype.emitQuery = function (e) {
   const query = e.target.value;
+  if (!query.length) emitter.emit('clear:query');
   if (!this.specialHunts.includes(this.state) && query.length) emitter.emit(`search:${this.state}`, query);
   else if (this.specialHunts.includes(this.state)) emitter.emit('search:special', this.state);
 };
 
-Search.prototype.toggleSearchInterface = function (e) {
-  this.state = e.target.id;
-  const getInputValue = (state) => {
-    switch (state) {
-      case 'state':
-        return this.stateSelect.value;
-      case 'species':
-        return this.speciesSelect.value;
-      case 'special':
-        return this.specialSelect.value;
-      case 'zipcode':
-        return this.input.value;
-      case 'facility':
-        return this.input.value;
-      default:
-        return this.state;
-    }
-  };
+Search.prototype.toggleSearchInterface = function (state) {
+  this.state = state;
   const eventName = this.specialHunts.includes(this.state) ? 'special' : this.state;
-  const value = getInputValue(this.state);
+  const value = this.getInput(this.state).value;
   emitter.emit(`search:${eventName}`, value);
   this.input.focus();
 };
+
+Search.prototype.getInput = function (state) {
+  switch (state) {
+    case 'state':
+      return this.stateSelect;
+    case 'species':
+      return this.speciesSelect;
+    case 'special':
+      return this.specialSelect;
+    case 'zipcode':
+      return this.input;
+    case 'facility':
+      return this.input;
+    default:
+      return this.state;
+  };
+}
 
 module.exports = Search;
