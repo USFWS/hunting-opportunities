6721e5b06011ea176148199c05f810a6f7457207
Roy Hewitt <roy_hewitt@fws.gov>
2020-02-19 15:21:32 -0500

    Convert to constructor; push new state into history to support back operation

--- src/js/deep-link.js
+++ src/js/deep-link.js
@@ -5,11 +5,55 @@ const { camelCase } = require('camel-case');
 const emitter = require('./emitter');
 const GLOBAL_BOUNDS = require('./bounds');
 
-const stateToBounds = (state) => GLOBAL_BOUNDS[camelCase(state)] || false;
-const isQuery = (obj) => ((obj.query) ? obj.query : false);
-const isSearchMethod = (obj) => ((obj.method) ? obj.method : false);
+const DeepLink = function (window) {
+  this.history = window.history;
+  this.window = window;
 
-const getBounds = (state) => {
+  this.window.addEventListener('popstate', this.historyHandler.bind(this));
+  emitter.on('search:state', (e) => this.updateHistory(e, 'state'));
+  emitter.on('search:species', (e) => this.updateHistory(e, 'species'));
+  emitter.on('search:zipcode', (e) => this.updateHistory(e, 'zipcode'));
+  emitter.on('search:special', (e) => this.updateHistory(e, 'special'));
+  emitter.on('search:facility', (e) => this.updateHistory(e, 'facility'));
+  emitter.on('clear:query', (e) => this.updateHistory(e))
+};
+
+// Maybe run an initialize function that sets up the query string when the map opens
+
+DeepLink.prototype.updateHistory = function (e, type) {
+  const params = {
+    method: type || querystring.parse(this.window.location.search).method,
+    ...(e && { query: e })
+  }
+  const string = querystring.stringify(params);
+  this.history.pushState(params, null, `/?${string}`);
+}
+
+DeepLink.prototype.historyHandler = function ({ state }) {
+
+  if (!state) return;
+
+  emitter.emit('update:search', {
+    ...(state.query && { query: state.query }),
+    ...(state.method && { method: state.method })
+  });
+};
+
+DeepLink.prototype.stateToBounds = function (state) {
+  return GLOBAL_BOUNDS[camelCase(state)] || false;
+};
+
+DeepLink.prototype.processQueryString = function (qs) {
+  const parsed = querystring.parse(qs);
+
+  if (parsed.state) emitter.emit('set:bounds', this.getBounds(parsed.state));
+  emitter.emit('update:search', {
+    ...(parsed.query && { query: parsed.query }),
+    ...(parsed.method && { method: parsed.method })
+  });
+};
+
+DeepLink.prototype.getBounds = function (state) {
   if (typeof state === 'string') return GLOBAL_BOUNDS[camelCase(state)];
   if (Array.isArray(state)) {
     return state
@@ -20,16 +64,4 @@ const getBounds = (state) => {
   return false;
 };
 
-const processQueryString = (qs) => {
-  const parsed = querystring.parse(qs);
-  const query = isQuery(parsed);
-  const searchMethod = isSearchMethod(parsed);
-
-  if (query) emitter.emit('query', query);
-  if (searchMethod) emitter.emit('method', searchMethod);
-  if (parsed.state) emitter.emit('set:bounds', getBounds(parsed.state));
-};
-
-module.exports = {
-  processQueryString,
-};
+module.exports = DeepLink;
