b3937dd5aa8ad9571a164df3f3703e07898c54f5
Roy Hewitt <roy_hewitt@fws.gov>
2020-10-13 10:19:17 -0400

    Display special opportunities, sort species list alphabetically

--- src/js/templates/hunt.js
+++ src/js/templates/hunt.js
@@ -2,20 +2,18 @@ const helpers = require('../helpers');
 
 const createListItem = (info) => {
   const regs = [info.MethodOfTake, info.DateTime, info.BagLimits].map(helpers.matchesStateRegs);
-  const isOpen = info.Access.includes('Open');
   return `
     <h4><strong>${info.Species}</strong></h4>
     <ul class="huntable-species-list">
       ${regs[0] ? '' : `<li>Method of take: <a href="${info.url}" target="_blank">${info.MethodOfTake}</a></li>`}
       ${regs[1] ? '' : `<li>Date & times: <a href="${info.url}" target="_blank">${info.DateTime}</a></li>`}
       ${regs[2] ? '' : `<li>Bag limit: <a href="${info.url}" target="_blank">${info.BagLimits}</a></li>`}
-      ${!isOpen ? `<li>Access: <a href="${info.url}" target="_blank">${info.Access}</a></li>` : '' }
+      ${info.SpecialOpportunities ? `<li><a href="${info.url}" target="_blank">${info.SpecialOpportunities}</a></li>` : '' }
     </ul>
   `;
 };
 
-module.exports = (props) => {
-  return `
+module.exports = (props) => `
   <button class="zoom-to-refuge hidden-button" value="${props.Organization_Code}">
     <h2>${props.Organization_Name}</h2>
   </button>
@@ -26,6 +24,8 @@ module.exports = (props) => {
   ${props.species && props.species.length ? '<h3>Huntable species</h3>' : ''}
   ${props.Huntable === 'Yes' ? '<p>State regulations for method of take, date/times and bag limit apply unless otherwise noted.</p>' : ''}
   ${props.species
+    .sort(helpers.byHuntableSpecies)
     .map((species) => createListItem({ ...species, url: props.facility.properties.UrlHunting }))
     .join('')
-}`};
+  }
+`;
